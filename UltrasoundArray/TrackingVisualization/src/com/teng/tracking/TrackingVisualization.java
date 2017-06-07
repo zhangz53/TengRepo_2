07283b4137dddac2be8af529330c2da94ba703cc
Teng Han <hanteng1021@outlook.com>
2017-06-07 18:43:21 +0900

    project setup

--- UltrasoundArray/TrackingVisualization/src/com/teng/tracking/TrackingVisualization.java
+++ UltrasoundArray/TrackingVisualization/src/com/teng/tracking/TrackingVisualization.java
@@ -1,5 +1,33 @@
 package com.teng.tracking;
 
-public class TrackingVisualization {
+import processing.core.PApplet;
+
+public class TrackingVisualization extends PApplet{
+	
+	public void settings(){
+		print("hello\n");
+		fullScreen();
+		
+    }
+
+    public void setup(){
+    	
+    	
+    }
+    
+    public void draw(){
+    	background(255);
+    }
+    
+    
+    public void keyPressed() {
+    	if (key == 'q') {
+    	    exit();
+    	}
+    }
+	
+	public static final void main(String args[]){
+		PApplet.main("com.teng.tracking.TrackingVisualization");
+	}
 
 }
