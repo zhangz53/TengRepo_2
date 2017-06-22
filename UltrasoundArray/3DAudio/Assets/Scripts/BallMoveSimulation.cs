ae846c5a006dd663de63c9e4eac0ab593826540f
Teng Han <hanteng1021@outlook.com>
2017-06-22 18:02:47 +0900

    first test.. no sound

---
+++ UltrasoundArray/3DAudio/Assets/Scripts/BallMoveSimulation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMoveSimulation : MonoBehaviour {
+
+	//use keyboard event to control the position of the ball object
+	//the center is 0,0,0 in the world coordinate
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.RotateAround(Vector3.zero, Vector3.up, 20 * Time.deltaTime);	
+	}
+}
